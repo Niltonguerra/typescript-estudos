0828883aa9a595886397098138a1e1a054ee5b17
Nilton Dionisio Guerra <78931445+Niltonguerra@users.noreply.github.com>
2024-04-27 07:48:09 -0300

    Delete src directory

--- src/utils/senhaCriptografada.ts
+++
@@ -1,9 +0,0 @@
-import crypto from "crypto";
-
-export const criaSenhaCriptografada = (senha: string) => {
-  const salt = crypto.randomBytes(16).toString("hex");
-  const hash = crypto.createHmac("sha256", salt);
-
-  hash.update(senha);
-  return hash.digest("hex");
-};
